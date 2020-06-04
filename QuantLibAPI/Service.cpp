2acfcd07682b70129ec6c19f1d2c7b5c74004742
Donex31 <astronomia31@gmail.com>
2020-06-04 19:41:57 +0200

    Rapier QuantLib

--- QuantLibAPI/Service.cpp
+++ QuantLibAPI/Service.cpp
@@ -1,9 +1,9 @@
 #include "Service.h"
 #include <string>
 #pragma warning( disable: 4819 )
-//#include <ql/quantlib.hpp>
+#include <ql/quantlib.hpp>
 
-//using namespace QuantLib;
+using namespace QuantLib;
 using std::string;
 
 void Service::initRestOpHandlers() {
@@ -29,98 +29,93 @@ void Service::handlePost(http_request message) {
 
                     std::cout << "S0 = " << S0 << " K = " << K << std::endl;
 
-                    //Calendar calendar = TARGET();
-                    //DayCounter dayCounter = Business252();
-                    //Date t0(20, Dec, 2011);
-                    //Settings::instance().evaluationDate() = t0;
-                    //Date T(26, Mar, 2012);
+                    calendar calendar = target();
+                    daycounter daycounter = business252();
+                    date t0(20, jan, 2020);
+                    settings::instance().evaluationdate() = t0;
+                    date t(26, mar, 2021);
 
-                    //Option::Type type(Option::Call);
-                    //Real S0 = val[U("S0")].as_number().to_double();
-                    //Real K = val[U("K")].as_number().to_double();
+                    Option::Type type(Option::Call);
+                    Real S0 = val[U("S0")].as_number().to_double();
+                    Real K = val[U("K")].as_number().to_double();
 
-                    //Spread q = val[U("Q")].as_number().to_double();
-                    //Rate r = val[U("r")].as_number().to_double();
+                    Spread q = val[U("Q")].as_number().to_double();
+                    Rate r = val[U("r")].as_number().to_double();
 
-                    //Volatility sigma = val[U("sigma")].as_number().to_double();
+                    Volatility sigma = val[U("sigma")].as_number().to_double();
 
-                    //Handle<Quote> underlyingH(
-                    //    boost::shared_ptr<Quote>(
-                    //        new SimpleQuote(S0)
-                    //        )
-                    //);
+                    Handle<Quote> underlyingH(
+                        boost::shared_ptr<Quote>(
+                            new SimpleQuote(S0)
+                            )
+                    );
 
-                    //Handle<YieldTermStructure> flatDividendTS(
-                    //    boost::shared_ptr<YieldTermStructure>(
-                    //        new FlatForward(t0, q, dayCounter)
-                    //        )
-                    //);
+                    Handle<YieldTermStructure> flatDividendTS(
+                        boost::shared_ptr<YieldTermStructure>(
+                            new FlatForward(t0, q, dayCounter)
+                            )
+                    );
 
-                    //Handle<YieldTermStructure> flatTermStructure(
-                    //    boost::shared_ptr<YieldTermStructure>(
-                    //        new FlatForward(t0, r, dayCounter)
-                    //        )
-                    //);
+                    Handle<YieldTermStructure> flatTermStructure(
+                        boost::shared_ptr<YieldTermStructure>(
+                            new FlatForward(t0, r, dayCounter)
+                            )
+                    );
 
-                    //Handle<BlackVolTermStructure> flatVolTS(
-                    //    boost::shared_ptr<BlackVolTermStructure>(
-                    //        new BlackConstantVol(t0, calendar, sigma, dayCounter)
-                    //        )
-                    //);
+                    Handle<BlackVolTermStructure> flatVolTS(
+                        boost::shared_ptr<BlackVolTermStructure>(
+                            new BlackConstantVol(t0, calendar, sigma, dayCounter)
+                            )
+                    );
 
-                    //boost::shared_ptr<BlackScholesMertonProcess> bsmProcess(
-                    //    new BlackScholesMertonProcess(underlyingH, flatDividendTS, flatTermStructure, flatVolTS)
-                    //);
+                    boost::shared_ptr<BlackScholesMertonProcess> bsmProcess(
+                        new BlackScholesMertonProcess(underlyingH, flatDividendTS, flatTermStructure, flatVolTS)
+                    );
 
-                    //boost::shared_ptr<Exercise> europenExercise(
-                    //    new EuropeanExercise(T)
-                    //);
+                    boost::shared_ptr<Exercise> europenExercise(
+                        new EuropeanExercise(T)
+                    );
 
-                    //boost::shared_ptr<StrikedTypePayoff> payoff(
-                    //    new PlainVanillaPayoff(type, K)
-                    //);
+                    boost::shared_ptr<StrikedTypePayoff> payoff(
+                        new PlainVanillaPayoff(type, K)
+                    );
 
-                    //VanillaOption europenOption(payoff, europenExercise);
+                    VanillaOption europenOption(payoff, europenExercise);
 
                     if (pathEndpoint == "blacksholes") {
                         std::cout << "POST api/v1/blacksholes"<<std::endl;
 
-                        //europenOption.setPricingEngine(
-                        //    boost::shared_ptr<PricingEngine>(
-                        //        new AnalyticEuropeanEngine(bsmProcess)
-                        //        )
-                        //);
+                        europenOption.setPricingEngine(
+                            boost::shared_ptr<PricingEngine>(
+                                new AnalyticEuropeanEngine(bsmProcess)
+                                )
+                        );
 
-                        //Real OptionPrice = europenOption.NPV();
-
-                        double OptionPrice = 25.65;
+                        Real OptionPrice = europenOption.NPV();
 
                         message.reply(status_codes::OK, OptionPrice);
                     }
 
                     if (pathEndpoint == "montecarlo") {
                         std::cout << "POST api/v1/blacksholes" << std::endl;
-                        //europenOption.setPricingEngine(
-                        //    boost::shared_ptr<PricingEngine>(
-                        //        new MCEuropeEngine<PseudoRandom>(
-                        //            bsmProcess,
-                        //            val[U("timestamps")].as_number().to_int32(),
-                        //            Null<Size>(),
-                        //            false,
-                        //            false,
-                        //            val[U("samples")].as_number().to_int32(),
-                        //            Null<Real>(),
-                        //            Null<Size>(), 
-                        //            SeedGenerator::instance().get()
-                        //            )
-                        //        )
-                        //);
+                        europenoption.setpricingengine(
+                            boost::shared_ptr<pricingengine>(
+                                new mceuropeengine<pseudorandom>(
+                                    bsmprocess,
+                                    val[u("timestamps")].as_number().to_int32(),
+                                    null<size>(),
+                                    false,
+                                    false,
+                                    val[u("samples")].as_number().to_int32(),
+                                    null<real>(),
+                                    null<size>(), 
+                                    seedgenerator::instance().get()
+                                    )
+                                )
+                        );
 
-                        //Real OptionPrice = europenOption.NPV();
-                        //Real Error = europenOption.errorEstimate();
-
-                        double OptionPrice = 25.65;
-                        double Error = 0.003454;
+                        real OptionPrice = europenoption.npv();
+                        real error = europenoption.errorestimate();
 
                         message.reply(status_codes::OK, json::value::number(OptionPrice));
                     }
